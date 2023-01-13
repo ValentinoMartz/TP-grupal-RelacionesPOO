7977391ca6e757ab209da7b9138896be604ed262
ValentinoMartz <valentinomartinez55@gmail.com>
2023-01-13 16:18:31 -0300

    objetos creados

--- LibFactura/LibFactura/Producto.cs
+++ LibFactura/LibFactura/Producto.cs
@@ -7,18 +7,24 @@ using System.Threading.Tasks;
 namespace LibFactura
 {
     public class Producto
-    {   
+    {
         public Producto() { }
-        public Producto(string nombre, decimal precio) 
+        public Producto(string nombre, decimal precio)
         {
-            Nombre= nombre;
-            Precio= precio;
+            Nombre = nombre;
+            Precio = precio;
         }
         public string Nombre { get; set; }
-        public decimal Precio { get; set;}
+        public decimal Precio { get; set; }
         public Categoria Categoria { get; set; }
         public List<DetalleFactura> DetalleFacturas { get; set; }
+
+
+
+        Producto producto1 = new Producto("PC", 199000);
+
+        Producto producto2 = new Producto("Copa del mundo", 40000);
+
     }
 
-    
 }
