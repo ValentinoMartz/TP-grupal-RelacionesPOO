37e3c723ccfa1633de0a1e31edbaebdf1d69ac00
Hlorenzoo <hernanlorenzo97@hotmail.com>
2023-01-13 14:34:38 -0300

    clases

---
+++ LibFactura/LibFactura/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibFactura
+{
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public decimal Precio { get; set;}
+        public Categoria Categoria { get; set; }
+    }
+}
